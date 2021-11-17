be863935770d74d57aff2d46854c93693b07bc0f
Andrey Mukhamedov <amukhamedov@flo.team>
2021-11-17 15:21:14 +0300

    feat: move methods from 72andSunny sls

--- collectionMicrosite.service.js
+++ collectionMicrosite.service.js
@@ -1,49 +1,53 @@
 const Helper = require('./helper/helper.js');
 
 module.exports = class CollectionMicrositeService {
-    iconik;
-    constructor() {
-        this.iconik = Helper.prototype.createIconikService();
-    }
-    async getAssetUrls(assetId) {
-        const assetURLS = {
-            proxyURL: '',
-            sourceURL: '',
-            keyframeURL: '',
-        };
-        try {
-            const proxy = await this.iconik.files.getProxiesOfAssetById(assetId);
-            const keyframes = await this.iconik.files.getAssetKeyframes(assetId);
+  iconik;
 
-            const proxyURL = proxy.objects[0].url;
-            const sourceURL = await this.getOriginSourceUrl(assetId);
-            const keyframeURL = keyframes.objects?.find((keyframe) => keyframe.type === 'KEYFRAME')?.url;
-            console.log('keyframe url', keyframeURL);
-            return {
-                proxyURL,
-                sourceURL,
-                keyframeURL,
-            };
-        } catch (error) {
-            console.log(`Asset(${assetId}) has not been updated`);
-            return assetURLS;
-        }
-    }
+  constructor() {
+    this.iconik = Helper.prototype.createIconikService({}, { fallbackApps: true, concurrent: 10 });
+  }
 
-    async getOriginSourceUrl(assetId) {
-        try {
-            const format = await this.iconik.files.getFormatsByAssetId(assetId);
-            const originalFormat = format.objects.find((i) => i.name === 'ORIGINAL');
-            const assetFiles = await this.iconik.files.getFilesOfAssetById(assetId, {
-                generate_signed_url: true,
-            });
-            const originalFile = assetFiles.objects.find((asset) => asset.format_id === originalFormat.id && asset.url);
-            const { url: sourceURL } = originalFile;
-            console.log('asset & url', assetId, sourceURL);
-            return sourceURL || '';
-        } catch (error) {
-            console.log(`Asset(${assetId}) has not been updated`);
-            return '';
-        }
+  async getAssetUrls(assetId) {
+    const assetURLS = {
+      proxyURL: '',
+      keyframeURL: '',
+      sourceURL: '',
+    };
+
+    try {
+      const [proxy, keyframes, sourceURL] = await Promise.all([
+        this.iconik.files.getProxiesOfAssetById(assetId),
+        this.iconik.files.getAssetKeyframes(assetId),
+        this.getOriginSourceUrl(assetId)]);
+      const proxyURL = proxy.objects[0]?.url || '';
+      const keyframeURL = keyframes.objects?.find((keyframe) => keyframe.type === 'KEYFRAME')?.url || '';
+
+      console.log('keyframe url', keyframeURL);
+      return {
+        proxyURL,
+        sourceURL,
+        keyframeURL,
+      };
+    } catch (error) {
+      console.log(`Asset(${assetId}) has not been updated`);
+      return assetURLS;
     }
+  }
+
+  async getOriginSourceUrl(assetId) {
+    try {
+      const format = await this.iconik.files.getFormatsByAssetId(assetId);
+      const originalFormat = format.objects.find((i) => i.name === 'ORIGINAL');
+      const assetFiles = await this.iconik.files.getFilesOfAssetById(assetId, {
+        generate_signed_url: true,
+      });
+      const originalFile = assetFiles.objects.find((asset) => asset.format_id === originalFormat?.id && asset.url);
+      console.log('asset & url', assetId, originalFile.url);
+      return originalFile?.url ? originalFile.url : 'empty';
+
+    } catch (error) {
+      console.log(`Asset(${assetId}) has not been updated`);
+      return '';
+    }
+  }
 }
--- helper/helper.js
+++ helper/helper.js
@@ -1,16 +1,14 @@
 const { IconikService } = require('@workflowwin/iconik-api');
 
 module.exports = class Helper {
-    createIconikService(options = null, request = null) {
-        const iconikOptions = {
-            appId: process.env.APP_ID,
-            authToken: request
-                ? request.headers['Auth-Token'] || request.headers['auth-token']
-                : process.env.AUTH_TOKEN,
-            iconikUrl: process.env.ICONIK_URL,
-            ...options,
-        };
+  createIconikService(options = null, request = null) {
+    const iconikOptions = {
+      appId: process.env.APP_ID,
+      authToken: process.env.AUTH_TOKEN,
+      iconikUrl: process.env.ICONIK_URL,
+      ...options,
+    };
 
-        return new IconikService(iconikOptions);
-    }
+    return new IconikService(iconikOptions, request);
+  }
 }
